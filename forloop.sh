e046f2515bb499b6353a069aa0027b7c2fd5735f
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2020-12-30 16:34:26 +0530

    First commit

---
+++ forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+for file in `ls *.txt`
+do
+	folderName=`echo $file | awk -F. '{print $1}'`;
+	if [ -d $folderName ]
+	then
+		rm -R $folderName
+	fi
+	mkdir $folderName;
+	cp $file $folderName;
+done
