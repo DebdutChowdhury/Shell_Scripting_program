5faa052f76f18651522b3e8cc3de6639d20e17aa
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-09 16:23:40 +0530

    Added: shell scripting program

--- arays/rand_aray.sh
+++ arays/rand_aray.sh
@@ -11,17 +11,6 @@ rand8=$(( 100 + RANDOM%999 ))
 rand9=$(( 100 + RANDOM%999 ))
 rand10=$(( 100 + RANDOM%999 ))
 
-#num[0]=$rand1
-#num[1]=$rand2
-#num[2]=$rand3
-#num[3]=$rand4
-#num[4]=$rand5
-#num[5]=$rand6
-#num[6]=$rand7
-#num[7]=$rand8
-#num[8]=$rand9
-#num[9]=$rand10
-#echo ${num[*]}
 numbers=($rand1 $rand2 $rand3 $rand4 $rand5 $rand6 $rand7 $rand8 $rand9 $rand10)
 max=${numbers[0]}
 min=${numbers[0]}
